050f003cc32f8a35123a37501e8c2b82337ee0f7
Jayden Callan <jqcallan04@gmail.com>
2022-10-01 12:16:17 -0600

    initial commit

---
+++ OpenWeather/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.Write("Enter your API key:  ");
+            var apiKey = Console.ReadLine();
+
+            string city;
+            do
+            {
+                Console.WriteLine();
+                Console.Write("Enter the city:  ");
+                city = Console.ReadLine();
+                if (city == "") { break; }
+                var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}";
+
+                var response = client.GetStringAsync(url).Result;
+
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+            } while (true);
+        }
+    }
+}
